9e414bfbab7fadb8d21717a2bc50f1c2516a2ab1
netodantaspe <110497895+netodantaspe@users.noreply.github.com>
2022-08-15 23:21:22 -0300

    Add files via upload

---
+++ exception.js
@@ -0,0 +1,18 @@
+public static void main(String args[])
+{
+  String frase = null;
+  String novaFrase = null;
+  try
+  {
+    novaFrase = frase.toUpperCase();
+  }
+  catch(NullPointerException e)
+  {
+    System.out.println("O frase inicial está nula,
+    para solucional tal o problema, foi lhe atribuito um valor default.");
+    frase = "Frase vazia";
+    novaFrase = frase.toUpperCase();
+  }
+  System.out.println("Frase antiga: "+frase);
+  System.out.println("Frase nova: "+novaFrase);
+}
