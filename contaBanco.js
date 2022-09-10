34d242a0c9432878aacc22c5dbf942fc6c21f3a3
netodantaspe <110497895+netodantaspe@users.noreply.github.com>
2022-09-10 20:45:21 -0300

    Add files via upload

---
+++ contaBanco.js
@@ -0,0 +1,14 @@
+nomeDoObjeto.nomedoMetodo = nomedafuncao;
+
+var meuObjeto = {
+meuMetodo: function(parametros) {}
+};
+
+objeto.nomedoMetodo(parametros);
+
+function Banco(saldo, deposito, saque, numeroConta){
+this.saldo = saldo;
+this.deposito = deposito;
+this.saque = saque;
+this.numeroConta = numeroConta
+}
